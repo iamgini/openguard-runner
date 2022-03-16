6befa1f75f8f0d6899b08b5a2b6b7ac52a0a6ecb
ginigangadharan <net.gini@gmail.com>
2022-03-16 19:32:29 +0800

    update

--- run_fix_jobs.py
+++ run_fix_jobs.py
@@ -4,9 +4,10 @@ import time
 import urllib.request
 from urllib.request import urlopen
 import json
-
+import requests
 from datetime import datetime
 import ansible_runner
+
 
 openguard_job_api_url = 'http://192.168.56.1:8000/api/incident_fix/'
 check_for_jobs = True
@@ -19,30 +20,56 @@ def fetch_job():
     #source = requests.get(openguard_job_api_url).json()
     data = urllib.request.urlopen(openguard_job_api_url).read()
     output = json.loads(data)
-    print(output)
-    this_hostname = output['managed_node']
-    node_connection_name = output['node_connection_name']
-    rule_fix_playbook = output['rule_fix_playbook']
+    #print(output)
+    pending_incidents =  output['pending_incidents']
+    
+    if pending_incidents == "YES":
+      this_hostname = output['managed_node']
+      node_connection_name = output['node_connection_name']
+      rule_fix_playbook = output['rule_fix_playbook']
+      this_incident_id = output['incident_id']
+      if rule_fix_playbook != "NA":
+        ## Call Ansible Runner
+        ansible_output = run_ansible(this_hostname, node_connection_name,rule_fix_playbook)
+        #print(ansible_output)
+        if ansible_output == 0:
+          try:
+            #pass
+            incident_status = 'FIXED'
+            incident_time_fixed = datetime.now()
+            incident_fix_comments = 'Updated item ' + str(this_incident_id)
+            #incident_id = this_incident_id
+            job_completed_data = { "incident_id": str(this_incident_id),
+                                        "incident_time_fixed": str(incident_time_fixed),
+                                        "incident_status": incident_status,
+                                        "incident_fix_comments": incident_fix_comments,
+                                      }
+            #print(job_completed_data)                                  
+            #job_completed_data_new =  json.load(job_completed_data)
+            #headers = {'Authorization' : ‘(some auth code)’, 'Accept' : 'application/json',    'Content-Type' : 'application/json'}
+            headers = {'Content-Type' : 'application/json'}
 
-    ## Call Ansible Runner
-    ansible_output = run_ansible(this_hostname, node_connection_name,rule_fix_playbook)
-    print(ansible_output)
-    if ansible_output == 0:
-      try:
-        pass
-        #incident.incident_status = 'FIXED'
-        #incident.incident_time_fixed = timezone.now
-        #incident.incident_fix_comments = 'Updated item ' + str(incident.id)
-        #incident.save()
-      except Exception as incident_entry_update_exception:
-        print(incident_entry_update_exception)
+            post_job_update = requests.post(openguard_job_api_url, json=job_completed_data,     headers=headers)
+            #print(job_completed_data)
+            #print(post_job_update.json())
+            print(f"Status Code: {post_job_update.status_code}, Response: {post_job_update. json ()}")
 
+            #incident.save()
+          except Exception as incident_entry_update_exception:
+            print(incident_entry_update_exception)
+
+          dateTimeObj = datetime.now()
+          timestampStr = str(dateTimeObj.strftime("%Y-%b-%d-%H:%M:%S"))
+
+          new_log = open( 'application_logs/logs', "a")
+          new_log.write( timestampStr + ": " + str(this_hostname)  + " - executed " + str   (rule_fix_playbook) + '\n')
+          #new_log.write('\n'  + json.dumps( incident.    #incident_time_reported ))
+          new_log.close()
+    else:
       dateTimeObj = datetime.now()
       timestampStr = str(dateTimeObj.strftime("%Y-%b-%d-%H:%M:%S"))
-
       new_log = open( 'application_logs/logs', "a")
-      new_log.write('\n' + timestampStr + ": " + str(this_hostname)  + " - executed " + str(rule_fix_playbook))
-      #new_log.write('\n'  + json.dumps( incident.    #incident_time_reported ))
+      new_log.write( timestampStr + ": No incidents found" + '\n')
       new_log.close()
 
   except Exception as fetch_data_exeception:
@@ -102,23 +129,17 @@ def run_ansible(node_names, ansible_host_name, playbook_file):
     #print(ansiblerunner.stats)
     return ansiblerunner.rc
       
-fetch_job()
+#fetch_job()
 
-#while check_for_jobs:
-#  try:
-#    
-#    #with urllib.request.urlopen(openguard_job_api_url) as get_data:
-#      get_data = urlopen(openguard_job_api_url)
-#      print(get_data.read().decode('utf-8'))
-#      print(type(get_data.read().decode('utf-8')))
-#      
-#      get_response = json.loads(get_data.read())
-#      print(type(type(get_response)))
-#    #print('hello')
-#
-#      time.sleep(check_interval)
-#  except Exception as fetch_data_exeception:
-#    print("Unable to fetch data.")
-#    print(fetch_data_exeception)
-#    time.sleep(check_interval)
-  
+#while True:
+#  time.sleep(check_interval)
+#  fetch_job()
+
+try:
+    while True:
+        fetch_job()
+        time.sleep(check_interval)
+        
+except KeyboardInterrupt:
+    print("Press Ctrl-C to terminate the script")
+    pass
